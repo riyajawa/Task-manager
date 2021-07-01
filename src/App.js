e99128a523577fe6517721db01f01c44e618f58d
riyajawa <riya1119.cse19@chitkara.edu.in>
2021-07-01 14:21:10 +0530

    reminder added

--- src/App.js
+++ src/App.js
@@ -33,11 +33,18 @@ function App() {
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !==id))
 }
-
+//toggle remainder
+const toggleReminder = (id) => {
+ setTasks(
+   tasks.map((task) => task.id === id ? {...task,reminder : !task.reminder} : task ))
+}
+ 
   return (
     <div className="container">
      <Heading />
-     <Tasks tasks ={tasks} onDelete = {deleteTask}/>
+     {tasks.length>0 ? <Tasks tasks ={tasks} onDelete = {deleteTask} 
+       onToggle ={toggleReminder}
+     />: ('No task')}
     </div>
   );
 }
